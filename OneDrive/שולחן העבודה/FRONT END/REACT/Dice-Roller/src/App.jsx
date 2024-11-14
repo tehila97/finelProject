10b9043d9c7b475e339d306ad20396fb613661a8
Your Name <youremail@example.com>
2024-11-14 08:47:04 +0200

    add 2 diceRoll

--- OneDrive/שולחן העבודה/FRONT END/REACT/Dice-Roller/src/App.jsx
+++ OneDrive/שולחן העבודה/FRONT END/REACT/Dice-Roller/src/App.jsx
@@ -4,12 +4,30 @@ import './App.css'
 function App() {
   const player = {
     name: "tehila",
-    cuttentScore: 0
+    currttentScore: 0
   }
   const targetScore = 50;
+
+  function throwDice () {
+    const num1 = Math.floor(Math.random() * 6 + 1);
+    console.log('num1', num1);
+    const num2 = Math.floor (Math.random () * 6 + 1);
+    console.log('num2',num2);
+    return [num1, num2];
+  }
+
+function handleDiceThrow () {
+  const [num1, num2] = throwDice();
+  console.log('num1', num1);
+  console.log('num12', num2);
+  player.currttentScore += (num1 + num2);
+  console.log('player.currttentScore', player.currttentScore);
+
+}
+
   return (
     <>
-    
+    <button onClick={handleDiceThrow}> handleDiceThrow</button>
     </>
   )
 }
