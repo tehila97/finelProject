da4ab205cda81700f61678cf703f98262f4558c5
tehila97 <tehila284@gmail.com>
2024-12-17 11:44:57 +0200

    Delete Downloads/core-concepts/01-starting-project directory

--- Downloads/core-concepts/01-starting-project/src/Components/CoreConcepts.jsx
+++
@@ -1,9 +0,0 @@
-export default function CoreConcepts ({image, title, description}) {
-    return (
-      <li>
-        <img src={image} alt={title}/>
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    );
-  }
--- Downloads/core-concepts/01-starting-project/src/Components/TabButton.jsx
+++
@@ -1,8 +0,0 @@
-export default function TabButton ({children, onSelect, isSelected, ...props}) {
-    return  (
-    <li> 
-        <button
-        className = {isSelected ? 'active' : undefined} {...props}> {children} </button>
-    </li>
-    );
-}
